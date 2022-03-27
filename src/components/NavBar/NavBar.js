7e367433c56d8dcfc7ee5c444b3ff4694f8c4ed3
Emanuel Diaz J <ema.leandro10@gmail.com>
2022-03-27 14:20:05 -0300

    Creando NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css'
+
+
+const NavBar = () => {
+    return(
+    <nav >
+        <ul class='nav__links' >
+            <li> <a href="#">COLECCIONES </a></li>
+            <li> <a href="#">UNITE A NOSOTROS </a></li>
+            <li> <a href="#">NIKE JOURNAL </a></li>
+        </ul>
+    </nav>    
+    
+    )
+  
+}
+
+
+export default NavBar
