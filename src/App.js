e7bf442db717bebe2fb5458acd275cd889fed576
Emanuel Diaz J <ema.leandro10@gmail.com>
2022-04-01 19:49:00 -0300

    Arreglo de Landing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Buttom from './components/Buttom/Buttom';
+
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={'./imagenes/logo nike.png'} className="App-logo" alt="logo" />  
-        <NavBar />
-        <ItemListContainer greeting= 'Hola Coder'/>
-        <Buttom />
-      </header>
+      <NavBar />
+      <ItemListContainer greeting='Hola Coder'/>
     </div>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,34 @@
 import './NavBar.css'
 import CartWidget from '../CartWidget/CartWidget'
+import Buttom from '../../components/Buttom/Buttom';
 
 const NavBar = () => {
-    return(
-    <nav >
-    <div>
-        <ul class='nav__links' >
-            <li> <a href=" ">COLECCIONES </a></li>
-            <li> <a href=" ">UNITE A NOSOTROS </a></li>
-            <li> <a href=" ">NIKE JOURNAL </a></li>
-        </ul>
-    </div>
-        
-    <div>
-        <CartWidget/>
-    </div>
-       
-    </nav>    
-    
+    return (
+        <nav className="navbar">
+            <div>
+                <img src={'./imagenes/logo nike.png'} className="App-logo" alt="logo" />
+            </div>
+
+            <div>
+                <ul class='nav__links' >
+                    <li> <a href=" ">COLECCIONES </a></li>
+                    <li> <a href=" ">UNITE A NOSOTROS </a></li>
+                    <li> <a href=" ">NIKE JOURNAL </a></li>
+                </ul>
+            </div>
+
+            <div>
+                <CartWidget />
+            </div>
+
+            <div>
+                <Buttom />
+            </div>
+
+        </nav>
+
     )
-  
+
 }
 
 
